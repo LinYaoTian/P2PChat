a8e245a01cd3d60112f9f3fc28f6e3a2826c3179
LinYaoTian <1552733075@qq.com>
2018-05-18 21:40:54 +0800

    传输文本消息

--- app/src/main/java/com/rdc/p2p/fragment/PeerListFragment.java
+++ app/src/main/java/com/rdc/p2p/fragment/PeerListFragment.java
@@ -63,7 +63,6 @@ public class PeerListFragment extends BaseFragment<PeerListPresenter> implements
                 case INIT_SERVER_SOCKET:
                     Log.d(TAG, "handleMessage: ");
                     mPresenter.initSocket();
-
                     break;
             }
             return true;
